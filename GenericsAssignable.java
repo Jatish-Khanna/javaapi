d38fc658cebc6983cd3195e04fb9e63a8583b33c
Jatish Khanna <jatish.khanna@gmail.com>
2020-02-17 10:05:28 +0530

    Update GenericsAssignable.java

--- GenericsAssignable.java
+++ GenericsAssignable.java
@@ -1,5 +1,18 @@
 
 
+/**
+  List<Object> is not super type of List<Pet> else you can add anything like Cat, Dog...
+  List<?> is super type of List<Pet> but with read only access, you cann't add anything to it
+  
+  List<? extends Pet> can add all the objects like Cat Dog this is possible because of Polymorphism
+  
+  Use<? extends> wild card only if you need to access the objects from the collection not updating it.
+  <? super ..> to add objects to the collection
+  <Type> if you need to both add and remove the data from the collection
+  
+  **/
+  
+
 public class GenericsAssignable {
  
     public static void main(String[] args) {

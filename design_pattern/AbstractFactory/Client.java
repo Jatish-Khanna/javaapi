0cda0e4e3429caeff3a3299ea3f6258070910a54
Jatish Khanna <jatish.khanna@gmail.com>
2020-04-08 09:30:11 +0530

    Create Client.java

---
+++ design_pattern/AbstractFactory/Client.java
@@ -0,0 +1,10 @@
+
+public class Client {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CharacterFactory factory = new RoboGunFactory();
+		factory.makeCharacter();
+	}
+}
